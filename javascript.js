57eab836b71fc6edae9f72003d80d83bcf7fbf4e
Josh Kling <joshuaskling@gmail.com>
2022-06-30 20:15:53 -0700

    added rps

--- javascript.js
+++ javascript.js
@@ -1,6 +1,32 @@
-console.log("Hello, World!")
-let message;
+console.log("Hello, World!");
 
-message = 'Hello'
+let message = 'Hello';
+let user = "John";
+let age = 25;
 
-alert(message);
+//alert(message);
+
+const MY_BIRTHDAY = "1987.07.13";
+
+//alert(MY_BIRTHDAY);
+
+function showMessage(){
+    let message = 'goodbye';
+    alert(message);
+}
+
+//showMessage();
+//alert(message);
+
+//fizzbuzz
+
+//user input
+const INPUT = parseInt(prompt("Please enter a number"));
+
+//loop through numbers
+for (let i = 0; i < INPUT; i += 1){
+    if (i % 3 === 0 && i % 5 === 0){console.log("Fizzbuzz");}
+    else if (i % 3 === 0){console.log("Fizz");}
+    else if (i % 5 === 0){console.log("Buzz");}
+    else {console.log(i);}
+}
